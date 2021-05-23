b234ec35bf7818f350362018835385288e9dfd75
stephaniec <75961904+Sec860@users.noreply.github.com>
2021-05-23 15:42:44 -0700

    Added temperature conversion to forecast

--- src/script.js
+++ src/script.js
@@ -30,9 +30,9 @@ function searchCity(event) {
 function formatTime(timestamp) {  
   let date = new Date(timestamp);
   let hours = date.getHours();
-    if (hours < 10) { `0${hours}`}
+  if (hours < 10) { `0${hours}`}
   let minutes = date.getMinutes();
-    if (minutes < 10) { `0${minutes}`}
+  if (minutes < 10) { `0${minutes}`}
   let weekday = weekdays[date.getDay()];
   let month = months[date.getMonth()];
   let day = date.getDate();
@@ -60,9 +60,9 @@ function getForecast(coordinates) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000)
   let day = weekdays[date.getDay()];
-
+  
   return day
- }
+}
 
 function showForecast(response) {
   let forecastElement= document.querySelector("#forecast")
@@ -98,6 +98,33 @@ function chooseCelsius(event) {
 function showCelsius(response) {
 temperature.innerHTML = `${Math.round(response.data.main.temp)}℃`
 document.querySelector("#wind").innerHTML = `${Math.round(response.data.wind.speed)} km/h`
+chooseForecastCelsius(response.data.coord)
+}
+
+function chooseForecastCelsius(coordinates) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(showForecastCelsius)
+}
+
+function showForecastCelsius(response) {
+  let forecastElement= document.querySelector("#forecast")
+  let forecast = response.data.daily
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function(forecastDay, index) {if (index < 6) {forecastHTML = forecastHTML + `
+        <div class="col-2 weekday">${formatDay(forecastDay.dt)} </br>
+        <img src=https://openweathermap.org/img/wn/${(forecastDay.weather[0].icon)}@2x.png
+        alt = "${forecastDay.weather[0].main} "
+        width = ""
+        />
+        <div class = "weekday-temperature">
+        <span class = "weekday-temperature-max">${Math.round(forecastDay.temp.max)}° </span> </br>
+        <span class = "weekday-temperature-min"> ${Math.round(forecastDay.temp.min)}°</span>
+        </div>
+        </div>
+       `
+}})
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML
 }
 
 fahrenheit.addEventListener("click", chooseFahrenheit)
@@ -112,7 +139,35 @@ function chooseFahrenheit(event) {
 function showFahrenheit(response) {
 temperature.innerHTML = `${Math.round(response.data.main.temp)}°F`
 document.querySelector("#wind").innerHTML = `${Math.round(response.data.wind.speed)} mph`
+chooseForecastFahrenheit(response.data.coord)
 }
+
+function chooseForecastFahrenheit(coordinates) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`
+  axios.get(apiUrl).then(showForecastFahrenheit)
+}
+
+function showForecastFahrenheit(response) {
+  let forecastElement= document.querySelector("#forecast")
+  let forecast = response.data.daily
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function(forecastDay, index) {if (index < 6) {forecastHTML = forecastHTML + `
+        <div class="col-2 weekday">${formatDay(forecastDay.dt)} </br>
+        <img src=https://openweathermap.org/img/wn/${(forecastDay.weather[0].icon)}@2x.png
+        alt = "${forecastDay.weather[0].main} "
+        width = ""
+        />
+        <div class = "weekday-temperature">
+        <span class = "weekday-temperature-max">${Math.round(forecastDay.temp.max)}° </span> </br>
+        <span class = "weekday-temperature-min"> ${Math.round(forecastDay.temp.min)}°</span>
+        </div>
+        </div>
+       `
+}})
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML
+}
+
 
 let geolocation = document.querySelector("#geolocation");
 geolocation.addEventListener("click", getPosition)
@@ -139,6 +194,7 @@ function showLocationWeather(response) {
   date.innerHTML = formatTime(response.data.dt * 1000)
   document.querySelector("#weather-icon").setAttribute("src", `https://openweathermap.org/img/wn/${(response.data.weather[0].icon)}@2x.png`)
   document.querySelector('#search-box').value = response.data.name;
+  getForecast(response.data.coord)
 
 }
 
